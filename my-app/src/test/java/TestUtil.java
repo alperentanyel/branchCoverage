fa0e0217682fd54e7f4fc985a898dab2d0a8c96d
alperentanyel <alperentanyel@gmail.com>
2024-03-27 01:30:37 +0300

    single argument tested

--- my-app/src/test/java/TestUtil.java
+++ my-app/src/test/java/TestUtil.java
@@ -1,16 +1,27 @@
+import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+
+
 
 import org.junit.Before;
 import org.junit.Test;
 
 public class TestUtil {
   Util c;
+  
 
   @Before
-  public void setUp() { c = new Util(); }
+  public void setUp() { c = new Util();  }
 
   @Test
   public void example() { assertTrue(true); }
+
+  @Test
+    public void testSingleArgumentReturnsFalse() {
+      int array []={5};
+        assertFalse(c.compute(array));
+    }
+
 }
